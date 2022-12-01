26f2f9cc092b933a9fbac3c4a7a3a06927105828
benpbo <prizeben@gmail.com>
2022-12-01 19:55:11 +0200

    feat: derive `Clone` trait for `Reply` and `Text`

--- src/ftp/reply.rs
+++ src/ftp/reply.rs
@@ -1,10 +1,10 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Reply {
     pub code: [u8; 3],
     pub text: Text,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Text {
     SingleLine {
         line: Vec<u8>,
